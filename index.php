6b3fc0a16cd5165e4ecb6b8a5db9a0430c5b134b
Hanif Eridaputra <he.23292@gmail.com>
2014-03-31 22:34:50 +0700

    style

--- index.php
+++ index.php
@@ -2,15 +2,61 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel='stylesheet' type="text/css" href="style.css"/>
 <title>Rute Angkot</title>
+
 </head>
 
-<body>
-lalala
-
-<div>
-	<iframe src="peta.html" width="605" height="340" name="categoryframe">  </iframe>
+<body class="main">
+<div class="header">
+	
 </div>
+
+
+
+<table width="100%">
+	<tr>
+		<td width="10%">
+			
+		</td>
+        <td align="center">
+			<a href="index.php"> LOGO </a>
+		</td>
+        <td align="center">
+			<a href="index.php"> HOME </a>
+		</td>
+        <td align="center">
+			ABOUT
+		</td>
+        <td width="10%">
+			
+		</td>
+	</tr>
+    
+    <tr class="bodyback">
+    	<td colspan="5">
+        	<table width="80%" align="center" class="bodymargin">
+            	<tr>
+					<td>
+        				<div>
+							<iframe src="peta.html" width="100%" height="500">  </iframe>
+						</div>
+					</td>
+                    <td width="20%" style="background-color:#FFD700;">
+						
+					</td>
+                    
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+	<tr>
+		<td colspan="5" style="background-color:#111111;text-align:center;">
+		lala
+        </td>
+	</tr>
+</table>
 
 </body>
 </html>
