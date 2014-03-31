8690382b181b370dcae4eeca2743ff38de1617cc
Trapsilo P. Bumi <traphole@gmail.com>
2014-04-01 00:49:04 +0700

    query pake php

--- query.php
+++ query.php
@@ -1,15 +1,31 @@
 <?php 
 $dbconn = pg_connect("host=localhost dbname=rute_angkot user=postgres password=root") or die("Could not connect.");
 
-$query = "SELECT jurusan, ST_AsText(geom) FROM rute";
-$result = pg_query($dbconn, $query) or die("Query failed: " . pg_last_error());
+$titik_asal = pg_fetch_result(pg_query($dbconn, "SELECT ST_GeomFromText('POINT(107.61378765106201 -6.886421743802813)')"), 0);
+$titik_tujuan = pg_fetch_result(pg_query($dbconn, "SELECT ST_GeomFromText('POINT(107.61863708496094 -6.905934772734723)')"), 0);
 
-foreach (pg_fetch_all($result) as $row) {
-	print_r($row);
-	// foreach ($row as $field => $value) {		
-	// }
+// $semua_jurusan = pg_fetch_all(pg_query($dbconn, "SELECT id, geom from rute"));
+
+// foreach ($semua_jurusan as $jurusan) {
+// }
+
+$jurusan_terdekat_asal_q = pg_query($dbconn, "SELECT id, geom, ST_Distance('".$titik_asal."',geom) AS distance FROM rute ORDER BY distance LIMIT 1");
+$jurusan_terdekat_asal_id = pg_fetch_result($jurusan_terdekat_asal_q, "id");
+$jurusan_terdekat_asal_geom = pg_fetch_result($jurusan_terdekat_asal_q, "geom");
+pg_free_result($jurusan_terdekat_asal_q);
+
+$jurusan_terdekat_tujuan_q = pg_query($dbconn, "SELECT id, geom, ST_Distance('".$titik_tujuan."',geom) AS distance FROM rute ORDER BY distance LIMIT 1");
+$jurusan_terdekat_tujuan_id = pg_fetch_result($jurusan_terdekat_tujuan_q, "id");
+$jurusan_terdekat_tujuan_geom = pg_fetch_result($jurusan_terdekat_tujuan_q, "geom");
+pg_free_result($jurusan_terdekat_tujuan_q);
+
+$equal_check = pg_fetch_result(pg_query($dbconn, "SELECT ST_Equals('" . $jurusan_terdekat_asal_geom . "', '" . $jurusan_terdekat_tujuan_geom . "')"), 0);
+
+if ($equal_check == 't') {
+	// return this route
+} else {
+	// lanjot
 }
 
-pg_free_result($result);
 pg_close($dbconn);
 ?>
