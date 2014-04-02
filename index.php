eea4951aaae5dfbba8b36f9556a3432365116034
Hanif Eridaputra <he.23292@gmail.com>
2014-04-02 22:57:54 +0700

    nambahin logo di icon web

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel='stylesheet' type="text/css" href="style.css"/>
 <title>Rute Angkot</title>
+<link rel="shortcut icon" href="LOGO.png" />
 
 </head>
 
