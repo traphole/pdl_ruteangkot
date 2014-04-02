8e9de95bd327fcb16e80cabe756e52223b9f1f08
Hanif Eridaputra <he.23292@gmail.com>
2014-04-02 22:36:27 +0700

    edit dikit

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel='stylesheet' type="text/css" href="style.css"/>
-<title>Rute Angkot</title>
+<title>About</title>
 
 </head>
 
@@ -11,16 +11,16 @@
 
 <table width="100%">
 	<tr>
-	  <td width="10%" class="header">
+	  	<td width="10%" class="header">
 			
-	  </td>
+	  	</td>
         <td width="35%">
 			<a href="index.php"> <img src="LOGO2.png" width="364" height="138" /> </a>
 		</td>
-        <td class="font" align="center">
+        <td width="22.5%" class="font" align="center">
 			<a href="index.php"> <b> HOME </b> </a>
 		</td>
-        <td class="font" align="center">
+        <td width="22.5%" class="font" align="center">
 			<a href="about.php"><b> ABOUT </b></a>
 		</td>
         <td width="10%">
@@ -29,8 +29,8 @@
 	</tr>
     
     <tr class="bodyback">
-    	<td colspan="5">
-        	<table height="580px" width="80%" align="center" class="bodymargin">
+    	<td height="580" colspan="5">
+        	<table width="80%" align="center" class="bodymargin">
             	<tr>
 				  	<td align="center">
                   		<img src="trap.jpg" width="339" height="339" />
--- index.php
+++ index.php
@@ -3,23 +3,24 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel='stylesheet' type="text/css" href="style.css"/>
-<title>About</title>
+<title>Rute Angkot</title>
+
 </head>
 
 <body class="main">
 
 <table width="100%">
 	<tr>
-	  <td width="10%" class="header">
+	  	<td width="10%" class="header">
 			
-	  </td>
+	  	</td>
         <td width="35%">
 			<a href="index.php"> <img src="LOGO2.png" width="364" height="138" /> </a>
 		</td>
-        <td class="font" align="center">
+        <td width="22.5%" class="font" align="center">
 			<a href="index.php"> <b> HOME </b> </a>
 		</td>
-        <td class="font" align="center">
+        <td width="22.5%" class="font" align="center">
 			<a href="about.php"><b> ABOUT </b></a>
 		</td>
         <td width="10%">
