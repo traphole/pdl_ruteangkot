92c75c17d40e9ed6bf87e0244069e1e0e4f643bf
Hanif Eridaputra <he.23292@gmail.com>
2014-04-03 12:58:41 +0700

    Tambah halaman semua rute

--- index.php
+++ index.php
@@ -18,11 +18,14 @@
         <td width="35%">
 			<a href="index.php"> <img src="LOGO2.png" width="364" height="138" /> </a>
 		</td>
-        <td width="22.5%" class="font" align="center">
-			<a href="index.php"> <b> HOME </b> </a>
+        <td width="15%" class="font" align="center">
+			<a href="index.php"> <b> PENCARIAN RUTE </b> </a>
 		</td>
-        <td width="22.5%" class="font" align="center">
-			<a href="about.php"><b> ABOUT </b></a>
+        <td width="15%" class="font" align="center">
+			<a href="allroutes.php"> <b> SEMUA RUTE </b> </a>
+		</td>
+        <td width="15%" class="font" align="center">
+			<a href="about.php"><b> TENTANG KAMI </b></a>
 		</td>
         <td width="10%">
 			
@@ -30,7 +33,7 @@
 	</tr>
     
     <tr class="bodyback">
-    	<td height="580px" colspan="5">
+    	<td height="580px" colspan="6">
         	<table width="80%" align="center" class="bodymargin">
             	<tr>
 				  <td>
@@ -38,8 +41,8 @@
 							<iframe class="frame" src="peta.html" width="100%" height="500">  </iframe>
 						</div>
 					</td>
-                    <td class="font" width="20%" bgcolor="#CBBBA0">
-						Untuk mencari rute dan angkot yang perlu dinaiki, tunjuk tempat asal dan tempat tujuan yang akan dicapai pada peta di samping lalu tekan cari.
+                    <td class="penjelasan" width="20%" bgcolor="#CBBBA0">
+						Untuk mencari rute dan angkot yang perlu dinaiki, tunjuk tempat asal dan tempat tujuan yang akan dicapai pada peta di samping lalu tekan Kalkulasi Rute.
 					</td>
                     
                 </tr>
@@ -48,7 +51,7 @@
     </tr>
 
 	<tr>
-		<td colspan="5" class="footer">
+		<td colspan="6" class="footer">
 		Copyright &copy; 2014 by AngkotWay Team.<br />
         All Rights reserved.
         </td>
